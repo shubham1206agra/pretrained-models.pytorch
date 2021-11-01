f6ded6549abd99a0f94c045f51bb19209a3011c8
Shubham Agrawal <58412969+shubham1206agra@users.noreply.github.com>
2021-11-01 12:54:06 +0530

    Update pnasnet.py
    Now using github release

--- pretrainedmodels/models/pnasnet.py
+++ pretrainedmodels/models/pnasnet.py
@@ -18,7 +18,7 @@ pretrained_settings = {
             'num_classes': 1000
         },
         'imagenet+background': {
-            'url': 'https://csciitd-my.sharepoint.com/:u:/g/personal/tt1191044_iitd_ac_in/EWKnN7BHqEVPlheW19OmqzkB9GLT4U6N8fgwtrxx8EV9HQ?download=1',
+            'url': 'https://github.com/shubham1206agra/pretrained-models.pytorch/releases/download/test/pnasnet5large-bf079911.pth',
             'input_space': 'RGB',
             'input_size': [3, 331, 331],
             'input_range': [0, 1],
